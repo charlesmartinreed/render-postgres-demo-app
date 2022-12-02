29935e42d84dda07d9079d4f05b372d1635d0b26
Charles Martin Reed <charlesmartinreed@icloud.com>
2022-12-02 15:31:50 -0600

    Add API paths for all employees, specific employee(s).

--- client/scripts/script.js
+++ client/scripts/script.js
@@ -1,25 +1,44 @@
 const listEl = document.querySelector("#list-test-data");
 
-async function fetchTest() {
+async function fetchAllEmployees() {
   try {
-    let URL = `http://localhost:7000/api`;
+    let URL = `http://localhost:7000/directory`;
     let payload = await fetch(URL);
     let data = await payload.json();
     return data;
   } catch (e) {
-    console.error(`Oh no, we couldn't fetch the data!`);
+    console.error(`Oh no, we couldn't fetch the data for all employees!`);
   }
 }
 
-window.addEventListener("DOMContentLoaded", async () => {
+async function fetchSingleEmployee(employeeName) {
+  try {
+    let URL = `http://localhost:7000/directory/${employeeName}`;
+    let payload = await fetch(URL);
+    let data = await payload.json();
+    return data;
+  } catch (e) {
+    console.error(
+      `Oh no, we couldn't fetch the data for this particular employee!`
+    );
+  }
+}
+
+async function displayAllEmployees() {
   let testData = await fetchTest();
   testData.forEach((datum) => {
-    let { name, department, email_address } = datum;
+    let {
+      person_name: { last_name, first_name },
+      person_department,
+      person_email_address,
+    } = datum;
     let listItemsHTML = `
-    <li class="list-test-data-item">Name: ${name}</li>
-    <li class="list-test-data-item">Department: ${department}</li>
-    <li class="list-test-data-item">Email: ${email_address}</li>
+    <li class="list-test-data-item">Name: ${first_name} ${last_name}</li>
+    <li class="list-test-data-item">Department: ${person_department}</li>
+    <li class="list-test-data-item">Email: ${person_email_address}</li>
     `;
     listEl.innerHTML += listItemsHTML;
   });
-});
+}
+
+displayAllEmployees();
--- server/app.js
+++ server/app.js
@@ -3,16 +3,37 @@ const app = express();
 const PORT = process.env.PORT || 7000;
 const CORS = require("cors");
 
+const staticEmployeeData = [
+  {
+    person_name: { last_name: "Reed", first_name: "Charles" },
+    person_department: "engineering",
+    person_email_address: "charles@averyrealcompany.co",
+  },
+  {
+    person_name: { last_name: "Summer", first_name: "Donna" },
+    person_department: "executive",
+    person_email_address: "donna@averyrealcompany.co",
+  },
+];
+
 app.use(CORS({ origin: ["http://localhost:5500"] }));
 
-app.get("/api", (req, res) => {
-  res.status(200).json([
-    {
-      name: "Charles",
-      department: "engineering",
-      email_address: "charles@averyrealcompany.co",
-    },
-  ]);
+app.get("/directory", (req, res) => {
+  res.status(200).json(staticEmployeeData);
 });
 
-app.listen(PORT, () => console.log(`Success! Now listening on PORT #${PORT} `));
+app.get("/directory/:employee", (req, res) => {
+  let nameParam = req.params.employee;
+
+  let result = staticEmployeeData.filter(
+    ({ person_name }) =>
+      person_name.last_name === nameParam ||
+      person_name.first_name === nameParam
+  );
+
+  res.status(200).json(result);
+});
+
+app.listen(PORT, () =>
+  console.log(`Success! API server now running on PORT #${PORT} `)
+);
